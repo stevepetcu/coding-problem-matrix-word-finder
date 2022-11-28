59b8870d3e5634ad1783fc8f6ea7629b721fc109
Stefan-Daniel Petcu <hello@stefanpetcu.com>
2022-11-28 22:09:53 +0000

    (SP) Application bootstrap.

---
+++ src/test/java/com/stefanpetcu/matrixwordfinder/MatrixWordFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanpetcu.matrixwordfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrixWordFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
